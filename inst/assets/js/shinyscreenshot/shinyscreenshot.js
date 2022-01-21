678896ef04945c4fd9fa8880ce5c0a2a354f6246
ferguskeatinge <12262076+ferguskeatinge@users.noreply.github.com>
2022-01-21 13:08:20 -0500

    Update shinyscreenshot.js

--- inst/assets/js/shinyscreenshot/shinyscreenshot.js
+++ inst/assets/js/shinyscreenshot/shinyscreenshot.js
@@ -11,7 +11,9 @@ var shinyscreenshot = {
         allowTaint: true,
         foreignObjectRendering: true,
         logging : false,
-        useCORS : true
+        useCORS : true,
+        height : window.outerHeight + window.innerHeight,
+        width : window.outerWidth + window.innerWidth
       }
     ).then(function(canvas) {
       var img = canvas.toDataURL();
